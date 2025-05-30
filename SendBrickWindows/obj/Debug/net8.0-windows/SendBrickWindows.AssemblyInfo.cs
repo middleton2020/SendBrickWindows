796737cdc25795fabf073e5f3cc03f6c96185aa2
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-05-30 21:29:15 +0100

    Finishe the primary setup.

--- SendBrickWindows/obj/Debug/net8.0-windows/SendBrickWindows.AssemblyInfo.cs
+++ SendBrickWindows/obj/Debug/net8.0-windows/SendBrickWindows.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SendBrickWindows")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78188771eaedf9c9acc080f2b56d0a7d546ad531")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09f50975074b0989f6e7a024d1fc6942cb39f2c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SendBrickWindows")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SendBrickWindows")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
