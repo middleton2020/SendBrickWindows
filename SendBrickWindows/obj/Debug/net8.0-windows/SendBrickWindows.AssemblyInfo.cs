1d899de7e9fcafb712c2163db5c2d61b0a8f43ab
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-06-02 21:27:59 +0100

    Create the first try at the Main login window. Still no fucntion to it.

--- SendBrickWindows/obj/Debug/net8.0-windows/SendBrickWindows.AssemblyInfo.cs
+++ SendBrickWindows/obj/Debug/net8.0-windows/SendBrickWindows.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SendBrickWindows")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09f50975074b0989f6e7a024d1fc6942cb39f2c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e8dfbe5997327d8060e8c9a8e63f309e717198")]
 [assembly: System.Reflection.AssemblyProductAttribute("SendBrickWindows")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SendBrickWindows")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
