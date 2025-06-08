211a38cf754640d2995b55e1456282a771eaf506
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-06-08 18:33:13 +0100

    Initial creation of main window.

--- SendBrickWindows/obj/Debug/net8.0-windows/SendBrickWindows.AssemblyInfo.cs
+++ SendBrickWindows/obj/Debug/net8.0-windows/SendBrickWindows.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SendBrickWindows")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e8dfbe5997327d8060e8c9a8e63f309e717198")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d899de7e9fcafb712c2163db5c2d61b0a8f43ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("SendBrickWindows")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SendBrickWindows")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
