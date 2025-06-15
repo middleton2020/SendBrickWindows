43071b69540e875dba3dcccf7998693f7326eb53
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-06-15 20:42:13 +0100

    Layout initial login window.

--- SendBrickWindows/MainWindow.xaml.cs
+++ SendBrickWindows/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SendBrickWindows
+namespace CM.SendBrickWindows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
