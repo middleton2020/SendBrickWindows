5f05043aea64d244ed17031e7a7cd4b0416ac5e2
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-12-24 18:29:04 +0000

    Reset to null and try again!

--- SendBrickWindows/MainWindow.xaml.cs
+++ SendBrickWindows/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using SendBrickWindows.Tools;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,56 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CM.SendBrickWindows
+namespace SendBrickWindows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private CurrentUser User { get; set; }
-
         public MainWindow()
         {
             InitializeComponent();
-            User = new CurrentUser();
-        }
-
-        private void DoLogin(object sender, RoutedEventArgs e)
-        {
-            // Define deligates for DoLogin and DoLogout.
-            // Attach DoLogout to the Click event.
-            if (User.LogInUser(UserName.Text, Password.Text))
-            {
-                BtnLogin.Content = "Logout";
-                BtnLogin.Click -= DoLogin;
-                BtnLogin.Click += DoLogout;
-
-                if (string.IsNullOrWhiteSpace(User.UserRecord.CompanyLogo))
-                {
-                    BitmapImage companyLogo = new BitmapImage();
-        companyLogo.BeginInit();
-                    companyLogo.UriSource= new Uri(User.UserRecord.CompanyLogo, UriKind.RelativeOrAbsolute);
-                    companyLogo.EndInit();
-
-        StoreLogo.Source = companyLogo;
-                }
-            }
-        }
-
-        private void DoLogout(object sender, RoutedEventArgs e)
-        {
-            if (User.IsLoggedIn())
-            {
-                BtnLogin.Content = "Login";
-                BtnLogin.Click -= DoLogout;
-                BtnLogin.Click += DoLogin;
-
-                StoreLogo.Source = SendBrickLogo.Source;
-            }
         }
     }
 }
--- SendBrickWindowsTest/obj/Debug/net8.0/SendBrickWindowsTest.AssemblyInfo.cs
+++ SendBrickWindowsTest/obj/Debug/net8.0/SendBrickWindowsTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SendBrickWindowsTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8a53acddba48b6e2a275ce27b9f24754897a372")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32bb38d924fbcb56962ef638586a2bbc62cbae9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SendBrickWindowsTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SendBrickWindowsTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
