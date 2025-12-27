917e685a02918c7a2d45a893608f798be80ea782
Colin Middleton <microsoft@medievalresearch.co.uk>
2025-12-27 20:34:50 +0000

    Add the UserModel class.

---
+++ SendBrickWindows/Models/UserModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.System;
+
+namespace CM.SendBrickWindows.Models
+{
+    class UserModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyLogo { get; set; }
+
+        public UserModel()
+        {
+
+        }
+        public UserModel(string userName)
+        {
+            UserName = userName;
+        }
+        public UserModel(string userName, string companyName)
+        {
+            UserName = userName;
+            CompanyName = companyName;
+        }
+        public UserModel(string userName, string companyName, string companyLogo)
+        {
+            UserName = userName;
+            CompanyName = companyName;
+            CompanyLogo = companyLogo;
+        }
+        public UserModel(string userId, string userName, string companyName, string companyLogo)
+        {
+            UserId = userId;
+            UserName = userName;
+            CompanyName = companyName;
+            CompanyLogo = companyLogo;
+        }
+    }
+}
