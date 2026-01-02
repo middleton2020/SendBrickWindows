56fc410ec3be33600c189a08ecf1d998324d8d5d
Colin Middleton <microsoft@medievalresearch.co.uk>
2026-01-02 21:25:18 +0000

    Create outline of server connection.

---
+++ SendBrickWindows/Tools/ServerConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CM.SendBrickWindows.Tools
+{
+    internal class ServerConnection
+    {
+        public ServerConnection()
+        {
+        }
+
+        public bool ConnectToServer()
+        {
+            return true;
+        }
+        public bool DisconnectFromServer()
+        {
+
+            return true;
+        }
+    }
+}
