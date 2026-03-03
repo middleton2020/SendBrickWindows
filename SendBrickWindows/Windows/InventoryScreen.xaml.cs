6c2e9b01a534ff6688305f5d99c18f54a742ea00
Colin Middleton <microsoft@medievalresearch.co.uk>
2026-03-03 20:23:53 +0000

    Add Inventory screen.

---
+++ SendBrickWindows/Windows/InventoryScreen.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CM.SendBrickWindows.Windows
+{
+    /// <summary>
+    /// Interaction logic for InventoryScreen.xaml
+    /// </summary>
+    public partial class InventoryScreen : Window
+    {
+        public InventoryScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
